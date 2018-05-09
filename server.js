2400bcfff448e20377c302eaf026c3dab6c2dfd6
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-08 23:04:33 -0400

    Changing ports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
