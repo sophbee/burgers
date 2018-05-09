f29cd9a0e2b9e80162dc69a2e58440e132f5aa11
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-08 23:17:15 -0400

    JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Setting up MySQL connection.
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
 // Making the connection.
 
